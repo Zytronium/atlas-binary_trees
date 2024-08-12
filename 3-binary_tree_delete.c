0ea0403dced7830daeca0ac362b832fac35d6377
Zytronium <dastelljes@outlook.com>
2024-08-12 18:57:46 -0500

    betty

--- 3-binary_tree_delete.c
+++ 3-binary_tree_delete.c
@@ -1,8 +1,6 @@
 #include "binary_trees.h"
 #include <stdio.h>
 
-/*binary_tree_t *get_root(binary_tree_t *node);*/
-
 /**
  * binary_tree_delete - deletes the entire given binary tree
  * @tree: the head node of the tree to delete
@@ -14,10 +12,8 @@ void binary_tree_delete(binary_tree_t *tree)
 
 	if (IS_LEAF(tree))
 	{
-		/*binary_tree_t *parent = tree->parent;*/
 		free(tree);
 		tree = NULL;
-		/*binary_tree_delete(parent);*/
 	}
 	else
 		if (tree->left != NULL)
@@ -25,10 +21,3 @@ void binary_tree_delete(binary_tree_t *tree)
 		else
 			binary_tree_delete(tree->right);
 }
-
-/*binary_tree_t *get_root(binary_tree_t *node)
-{
-	if (node->parent == NULL)
-		return (node);
-	return (get_root(node->parent));
-}*/
