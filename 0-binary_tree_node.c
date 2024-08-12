685999b35d71fd69d1a736a5e8052e3d56db9a93
Zytronium <dastelljes@outlook.com>
2024-08-12 16:17:44 -0500

    did task 0

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -3,8 +3,8 @@
 /**
  * binary_tree_node - creates a binary tree node
  *
- * @parent: descri
- * @value: 		  ption
+ * @parent: the parent node of the node to create
+ * @value: the value of n to put in the new node
  *
  * Return: pointer to the newly created node, or NULL if it failed
  */
@@ -14,6 +14,11 @@ binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 
 	if (newNode == NULL)
 		return (NULL);
+
+	newNode->parent = parent;
+	newNode->left = NULL;
+	newNode->right = NULL;
+	newNode->n = value;
 
 	return (newNode);
 }
