6c9b710b9e371d4ee2be0af8d0203c1420be1be2
Zytronium <dastelljes@outlook.com>
2024-08-12 23:26:49 -0500

    started task 6

---
+++ 6-binary_tree_preorder.c
@@ -0,0 +1,13 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_preorder - goes through a binary tree using pre-order
+ * traversal and performs the given function on each element's value
+ *
+ * @tree: 🌲tree🌲
+ * @func: function pointer for the function to perform on each element's value
+ */
+void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int))
+{
+
+}
